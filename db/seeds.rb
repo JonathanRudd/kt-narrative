3ed5729a096ad005d957a3824cf4536dea438569
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-10-16 00:43:31 +0900

    seed try 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Destroying all Killteams"
+Killteam.destroy_all
+
+puts "Destroying all Warzones"
+Warzone.destroy_all
+
+puts "Destroying all users"
+User.destroy_all
+
+puts "creating Jonathan"
+
+User.create!(
+  email: "jonathan@gmail.com",
+  password: "123456"
+)
+
+puts "creating Vengaboyz"
+
+Killteam.create!(
+  user_id: User.first.id,
+  name: "Vengaboyz",
+  description: "Ork Kommandoz emulating a group of umies they saw on an old mag tape",
+  requisition: "2",
+  assetcap: "4",
+  base: "Da Vengabus",
+  factionkeyword: "Kommandoz"
+)
